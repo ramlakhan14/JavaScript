ac39051b2262e0654c8b40776845580281fcc4c1
Ramlakhan Kumar <70676290+ramlakhan14@users.noreply.github.com>
2025-03-12 11:05:26 +0000

    Operations on datatype conversion

---
+++ basics/conversionOperations.js
@@ -0,0 +1,52 @@
+// let score = "33"
+//  console.log( typeof score);
+//  console.log( typeof (score));
+
+//  let valueInNumber = Number( score )
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber)
+
+// let score = "33abc"
+//  console.log( typeof score);
+//  console.log( typeof (score));
+
+//  let valueInNumber = Number( score )
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber)
+
+// let score = null
+//  console.log( typeof score);
+//  console.log( typeof (score));
+
+//  let valueInNumber = Number( score )
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber)
+
+// let score = undefined
+//  console.log( typeof score);
+//  console.log( typeof (score));
+
+//  let valueInNumber = Number( score )
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber)
+
+ //"33" => 33
+ //"33abc" => NaN
+ // true => 1; false=>0
+
+
+ //************************************ Operations ***************************************
+
+//  let value = 3;
+//  let negVlaue = -value;
+//  console.log(negVlaue)
+
+let str1 = "Hello"
+let str2 = " Ram"
+let str3 = str1 + str2
+console.log (str3)
+
+console.log( "1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") //32
